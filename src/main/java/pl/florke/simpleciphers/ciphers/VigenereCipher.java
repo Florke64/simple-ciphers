71d0b6f4443f51ed20cad609ba3bb68d3048c758
Florke64 <daniel@florke.pl>
2024-06-06 20:13:00 +0200

    ADD: Vigenere Cipher

--- src/main/java/pl/florke/simpleciphers/ciphers/VigenereCipher.java
+++ src/main/java/pl/florke/simpleciphers/ciphers/VigenereCipher.java
@@ -1,21 +1,43 @@
 package pl.florke.simpleciphers.ciphers;
 
 public class VigenereCipher implements Cipher {
-    private String key;
+    private char[] key;
 
     public VigenereCipher(String key) {
-        this.key = key;
+        this.key = key.toCharArray();
     }
 
     @Override
     public String encrypt(String plaintext) {
         // Implementation of Vigenere encryption
-        return "";
+        final char[] chars = plaintext.toCharArray();
+
+        int loopIndex = 0;
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] = (char) (chars[i] + key[loopIndex]);
+            loopIndex++;
+            if (loopIndex >= key.length) {
+                loopIndex = 0;
+            }
+        }
+
+        return new String(chars);
     }
 
     @Override
     public String decrypt(String ciphertext) {
         // Implementation of Vigenere decryption
-        return "";
+        final char[] chars = ciphertext.toCharArray();
+
+        int loopIndex = 0;
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] = (char) (chars[i] - key[loopIndex]);
+            loopIndex++;
+            if (loopIndex >= key.length) {
+                loopIndex = 0;
+            }
+        }
+
+        return new String(chars);
     }
 }
